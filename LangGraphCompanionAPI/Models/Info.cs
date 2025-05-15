4394e9ec2d3d3ea94e2ccf815cf2411be7551f39
Héctor López Hidalgo <data_analitics_hlh@protonmail.com>
2025-05-15 17:40:25 +0200

    removing small redundant keywords

--- LangGraphCompanionAPI/Models/Info.cs
+++ LangGraphCompanionAPI/Models/Info.cs
@@ -1,6 +1,29 @@
+using System.Runtime.InteropServices.Marshalling;
+
 namespace LangGraphCompanionAPI.Models;
 
-public struct Info
+public readonly struct PresentationInfo
 {
+    public string Message { get; }
+    public string Description { get; }
+    public  string Author { get; }
+    public  double Version { get; }
+    public  Dictionary<string, string> Endpoints { get; }
+    public  Dictionary<string, string> Metadata { get; }
     
+    public PresentationInfo(
+     string message,
+     string description,
+     string author,
+     double version,
+     Dictionary<string, string> endpoints,
+     Dictionary<string, string> metadata)
+        {
+        Message = message;
+        Description = description;
+        Author = author;
+        Version = version;
+        Endpoints = endpoints;
+        Metadata = metadata;
+        }
 }
--- LangGraphCompanionAPI/Models/TaskItem.cs
+++ LangGraphCompanionAPI/Models/TaskItem.cs
@@ -1,11 +1,14 @@
+
+
 namespace LangGraphCompanionAPI.Models
 {
-    public class TaskItem
+    public class TaskItem 
     {
-        public int Id { get; set;}
-        public string Name { get; set;}
-        public DateTime DueTime { get; set;}
-        public bool IsCompleted { get; set;}
-    
+   
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime dueDate { get; set; }
+        public bool isCompleted { get; set; }
+
     }
 }
